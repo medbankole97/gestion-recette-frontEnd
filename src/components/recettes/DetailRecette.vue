d7fd8fc8594a7f54abad504eb4bc151ec98efc51
aichetou1408 <aichetouahmedgaye@gmail.com>
2024-09-02 16:44:36 +0100

    Upadated store folder

--- src/components/recettes/DetailRecette.vue
+++ src/components/recettes/DetailRecette.vue
@@ -1,12 +1,55 @@
 <template>
-    <h1>hello</h1>
-  
-    
-  </template>
-  
-  <script setup>
-  </script>
-  
-  <style scoped>
-  </style>
-  
+  <div class="container">
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+      Launch static backdrop modal
+    </button>
+
+    <!-- Modal -->
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+      aria-labelledby="staticBackdropLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="staticBackdropLabel">Details de la recette:</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <div v-if="reset">
+              <p>{{ reset.title }}</p>
+              <p>{{ reset.ingredients }}</p>
+              <p>{{ reset.type }}</p>
+            </div>
+
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Understood</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+
+</template>
+
+<script setup>
+import { useRecetteStore } from '@store/'
+import { ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+
+const store = useRecetteStore()
+const route = useRoute()
+const router = useRouter()
+const reset = ref(null)
+
+const id = route.params.id
+
+const remoteRecette = () => {
+  reset.value = store.recettes.getById(id)
+}
+</script>
+
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas);
+import { fab } from '@fortawesome/free-brands-svg-icons';
+library.add(fab);
+import { far } from '@fortawesome/free-regular-svg-icons';
+library.add(far);
+import { dom } from "@fortawesome/fontawesome-svg-core";
+dom.watch();
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
@@ -7,6 +18,7 @@ import router from '@router'
 
 
 const app =createApp(App)
+app.component("font-awesome-icon", FontAwesomeIcon);
 const pinia = createPinia()
 app.use(pinia)
 app.use(router)
