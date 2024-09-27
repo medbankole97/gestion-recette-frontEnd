78c3ff240647c6ed594cc472b7b973c729be35ba
Mohamed Bankolé <medbankole97@gmail.com>
2024-09-27 06:21:17 +0000

    add component AjoutRecette

--- src/components/recettes/ListeRecette.vue
+++ src/components/recettes/ListeRecette.vue
@@ -1,14 +1,17 @@
 <template>
-  <nav class="navbar bg-white px-5">
+   <nav class="navbar bg-white px-5">
     <router-link to="/">
       <img src="/src/assets/logo.png" alt="Logo" class="logo">
     </router-link>
     <form class="d-flex justify-content-around px-5">
       <router-link to="/recette/patisserie" class="btn btn-outline-none me-3">{{ $t("recette.list.nav") }}</router-link>
       <router-link to="/recette/cuisine" class="btn btn-outline-none">{{ $t("recette.list.nav2") }}</router-link>
+      <div v-for="categorie in categories" :key="categorie.id">
+        <router-link :to="`/recette/categorie/${categorie.nom}`" class="btn btn-outline-none me-3">{{ categorie.nom }}</router-link>
+      </div>
     </form>
     <div>
-      <select id="langue" @change="change()">
+      <select id="langue" @change="changeLanguage">
         <option value="en">En</option>
         <option value="fr">Fr</option>
       </select>
@@ -102,7 +105,11 @@ const view = (recette) => {
 // const change() {
   
 // }
+const { locale } = useI18n();
 
+function changeLanguage(event)  {
+  locale.value = event.target.value;
+}
 
 </script>
 
