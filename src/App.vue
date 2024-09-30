1f63d3b8943e0cab7c014719d031c814bff9940f
aichetou1408 <aichetouahmedgaye@gmail.com>
2024-09-30 10:11:05 +0100

    Added active class

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,17 @@
   <div>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div class="container-fluid">
-        <router-link to="/" class="navbar-brand">
+        <router-link to="/" class="navbar-brand" activeClass="active">
           <img src="/src/assets/recipeLogo.jpg" alt="Logo" class="logo">
         </router-link>
 
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <router-link to="/recette" class="nav-link text-white">{{ $t("home.title1") }}</router-link>
+              <router-link to="/recette" class="nav-link text-white" activeClass="active">{{ $t("home.title1") }}</router-link>
             </li>
             <li class="nav-item">
-              <router-link to="/categorie" class="nav-link text-white">{{ $t("home.title2") }}</router-link>
+              <router-link to="/categorie" class="nav-link text-white" activeClass="active">{{ $t("home.title2") }}</router-link>
             </li>
           </ul>
         </div>
@@ -47,8 +47,12 @@ function changeLanguage(event) {
   height: 40px;
   width: auto;
 }
-
-
+.active{
+  font-weight: 600;
+  border-bottom: 3px solid rgb(165, 164, 164);
+  color: black;
+  border-radius: 10px;
+}
 .content {
   padding-top: 70px; 
 }
